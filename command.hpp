b49b0bbeac8b5a2b1d43aadabaa908ad8a20bf15
gray fox <gfox@localhost.localdomain>
2018-12-04 09:23:11 -0600

    updated with the latest version of command.hpp

--- command.hpp
+++ command.hpp
@@ -13,47 +13,28 @@ class command {
 public:
     command();
     virtual ~command();
-    // Fire and forget. Takes a terminal command as first arg, and set 2nd arg bool to true to see terminal output in real time, false for quiet.
-    void exec(const char *cmd, bool realtime_terminal_feedback);
-    // overloaded method. Takes two vectors of strings as args, and pushes all terminal output to the first one, and errors reported to the second one. same rules for original two args holds.
-    void exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback);
-	// overloaded method. Uses deques instead of vectors. No other differences.
-	void exec(const char *cmd, std::deque<std::string> &terminal_output, std::deque<std::string> &error_list, bool realtime_terminal_feedback);
+	
+	void toggle_debug();	// toggles the state of m_debug
+    void exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list);
+
+private:
+	bool m_debug;	// if true, realtime terminal output will be printed to screen as it happens. Otherwise, it goes to the vector, or is lost forever.
 };
 
 command::command() {}   // ctor
 command::~command() {}  // dtor
 
-void command::exec(const char* cmd, bool realtime_terminal_feedback) {
-    char buffer[128];
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) {
-        if (realtime_terminal_feedback)
-            std::cout << "popen() failed!" << std::endl;
-        throw::std::runtime_error("popen() failed!");
-    }
-    try {
-        while (!feof(pipe)) {
-            if (fgets(buffer, 128, pipe) != NULL) {
-                if (realtime_terminal_feedback)
-                    std::cout << buffer;
-            }
-        }
-    } catch (...) {
-        pclose(pipe);
-        if (realtime_terminal_feedback)
-            std::cout << "fgets failed!" << std::endl;
-        throw;
-    }
-    pclose(pipe);
+void command::toggle_debug() {
+	m_debug = !m_debug;
+	std::cout << "debug = [" << m_debug << "]" << std::endl;
 }
 
-void command::exec(const char* cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback) {
+void command::exec(const char* cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list) {
     char buffer[128];
     FILE* pipe = popen(cmd, "r");
     if (!pipe) {
         error_list.push_back("popen() failed!");
-        if (realtime_terminal_feedback)
+        if (m_debug)
             std::cout << "popen() failed!" << std::endl;
         throw::std::runtime_error("popen() failed!");
     }
@@ -62,42 +43,14 @@ void command::exec(const char* cmd, std::vector<std::string> &terminal_output, s
             if (fgets(buffer, 128, pipe) != NULL) {
                 std::string temp = buffer;
                 terminal_output.push_back(temp);
-                if (realtime_terminal_feedback)
+                if (m_debug)
                     std::cout << temp;
             }
         }
     } catch (...) {
         pclose(pipe);
         error_list.push_back("fgets failed!");
-        if (realtime_terminal_feedback)
-            std::cout << "fgets failed!" << std::endl;
-        throw;
-    }
-    pclose(pipe);
-}
-
-void command::exec(const char* cmd, std::deque<std::string> &terminal_output, std::deque<std::string> &error_list, bool realtime_terminal_feedback) {
-    char buffer[128];
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) {
-        error_list.push_back("popen() failed!");
-        if (realtime_terminal_feedback)
-            std::cout << "popen() failed!" << std::endl;
-        throw::std::runtime_error("popen() failed!");
-    }
-    try {
-        while (!feof(pipe)) {
-            if (fgets(buffer, 128, pipe) != NULL) {
-                std::string temp = buffer;
-                terminal_output.push_back(temp);
-                if (realtime_terminal_feedback)
-                    std::cout << temp;
-            }
-        }
-    } catch (...) {
-        pclose(pipe);
-        error_list.push_back("fgets failed!");
-        if (realtime_terminal_feedback)
+        if (m_debug)
             std::cout << "fgets failed!" << std::endl;
         throw;
     }
